2be7a303f5b9de67c28af2bfb2f97e19224e0d6d
devi5040 <dpraidola@gmail.com>
2022-08-14 09:24:07 +0530

    A simple program to understand the use of variables in C++

---
+++ var.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    name = "devi";
+    int age = 20;
+    cout << "There once was a man named " << name << endl;
+    cout << "He was " << age << " years old " << endl;
+    cout << "He liked the name " << name << endl;
+    cout << "But did not like being " << age << endl;
+    return 0;
+}
