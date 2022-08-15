cf52ae0636b1d69a91bfed2babf301727ee44326
devi5040 <dpraidola@gmail.com>
2022-08-15 08:45:53 +0530

    Simple C++ Program to understand Types in C++

---
+++ types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a = 'A';
+    string d = "Strings";
+    int inum = 30;
+    float fnum = 30.33;
+    double dnum = 50.50;
+    bool b = true;
+    cout << "Value of variable a: " << a << endl;
+    cout << "Value of variable d: " << d << endl;
+    cout << "Value of variable inum: " << inum << endl;
+    cout << "Value of variable fnum: " << fnum << endl;
+    cout << "Value of variable dnum: " << dnum << endl;
+    cout << "Value of variable b: " << b << endl;
+    return 0;
+}
