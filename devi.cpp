a6e89dec39615a41edea138451ba86457e285bc5
devi5040 <dpraidola@gmail.com>
2022-10-22 20:49:10 +0530

    one

---
+++ devi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+int main()
+{
+    int num = 3;
+    if (num > 0)
+    {
+        printf("Number is positive");
+    }
+    else
+    {
+        printf("Number is negative");
+    }
+}
